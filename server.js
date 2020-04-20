a2f92a5e349fc0484521390a3c56c8d2d52240b5
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-04-20 11:53:46 -0400

    Adding dependencies and run/watch scripts

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var exphbs = require('express-handlebars');
 var mysql = require('mysql');
-var moment = require('moment'); 
+var moment = require('moment');
 
 // First instantiation of express
 var app = express();
@@ -32,8 +32,38 @@ connection.connect(err => {
 });
 
 // ROUTES
+app.get("/birthdays", (req, res) => {
+    var family = "Fowose";
+
+    connection.query(`SELECT * FROM ${family}`, (err, data) => {
+        if (err) throw err;
+
+        // console.log(data);
+
+        var bdayData = [];
+
+        for (i = 0; i < data.length; i++) {
+            var currentBday = data[i].dob
+            var currentMomentBday = moment(currentBday).format("MMMM Do YYYY");
+
+            data[i] = {
+                id: data[i].id,
+                name: data[i].name,
+                dob: currentMomentBday
+            };
+
+            console.log(data[i]);
 
 
+            // console.log(currentMomentBday); 
+            bdayData.push(data[i]);
+
+        };
+        res.render("bdaze", { person: bdayData });
+
+    })
+
+})
 
 
 
