e5dacdd6492e1c4f56316683f5c23c2d3a1e9adc
Andrew Fowose <50899477+usafowose@users.noreply.github.com>
2020-04-23 01:27:49 -0400

    Changed hash (-) to backslashes and added additional input for dob field

--- schema.sql
+++ schema.sql
@@ -11,13 +11,13 @@ CREATE TABLE Fowose (
     PRIMARY KEY (id)
 ); 
 
-INSERT INTO Fowose (name, dob) VALUES ("Olakunle Fowose", '1956-06-15'); 
-INSERT INTO Fowose (name, dob) VALUES ("Jumoke Fowose", '1964-03-28'); 
-INSERT INTO Fowose (name, dob) VALUES ("Simeon Fowose", '1986-08-04'); 
-INSERT INTO Fowose (name, dob) VALUES ("Folajimi Fowose", '1991-05-26'); 
-INSERT INTO Fowose (name, dob) VALUES ("Mobolaji Fowose", '1992-10-15'); 
-INSERT INTO Fowose (name, dob) VALUES ("Olufikayo Fowose", '1994-12-10'); 
-INSERT INTO Fowose (name, dob) VALUES ("Andrew Fowose", '1997-03-17'); 
+INSERT INTO Fowose (name, dob) VALUES ("Olakunle Fowose", '1956/06/15'); 
+INSERT INTO Fowose (name, dob) VALUES ("Jumoke Fowose", '1964/03/28'); 
+INSERT INTO Fowose (name, dob) VALUES ("Simeon Fowose", '1986/08/04'); 
+INSERT INTO Fowose (name, dob) VALUES ("Folajimi Fowose", '1991/05/26'); 
+INSERT INTO Fowose (name, dob) VALUES ("Mobolaji Fowose", '1992/10/15'); 
+INSERT INTO Fowose (name, dob) VALUES ("Olufikayo Fowose", '1994/12/10'); 
+INSERT INTO Fowose (name, dob) VALUES ("Andrew Fowose", '1997/03/17'); 
 
 
 
